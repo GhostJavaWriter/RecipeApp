1ea93bff6a421e3faa64281f01abcc00013b817b
Bair <40758299+GhostJavaWriter@users.noreply.github.com>
2023-05-30 11:15:19 -0700

    initial commit

---
+++ RecipeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecipeApp
+//
+//  Created by Bair Nadtsalov on 30.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
