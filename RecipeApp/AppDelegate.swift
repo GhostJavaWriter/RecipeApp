1af6841edac2e24de86fc664925a73a044e8a0fb
Bair <40758299+GhostJavaWriter@users.noreply.github.com>
2023-05-31 10:17:15 -0700

    CaptionLabel: set as background

--- RecipeApp/AppDelegate.swift
+++ RecipeApp/AppDelegate.swift
@@ -14,6 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        let families = UIFont.familyNames
+        families.sorted().forEach {
+          print("\($0)")
+          let names = UIFont.fontNames(forFamilyName: $0)
+          print(names)
+        }
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
--- RecipeApp/ViewController.swift
+++ RecipeApp/ViewController.swift
@@ -9,12 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI
+    
+    private lazy var backgroundImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "background")
+        view.contentMode = .scaleAspectFit
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+//    private lazy var welcomeLabel: UILabel = {
+//        let label = UILabel()
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        label.adjustsFontForContentSizeCategory = true
+//        label.text = "What will\n we cook?"
+//        label.textColor = .white
+//        label.numberOfLines = 0
+//        return label
+//    }()
+    
+    // MARK: - Properties
+    
+//    var fontName = "Annabelle" {
+//        didSet {
+//            scaledFont = ScaledFont(fontName: fontName)
+//            configureFont()
+//        }
+//    }
+//
+//    private lazy var scaledFont: ScaledFont = {
+//        return ScaledFont(fontName: fontName)
+//    }()
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        configureView()
     }
 
+    // MARK: - Private methods
+    
+    private func configureView() {
+        
+        view.backgroundColor = UIColor(hex: "#E8B634")
+        
+        view.addSubview(backgroundImageView)
+        
+        NSLayoutConstraint.activate([
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+    }
+    
+//    private func configureFont() {
+//        welcomeLabel.font = scaledFont.font(forTextStyle: .largeTitle)
+//    }
 
 }
 
