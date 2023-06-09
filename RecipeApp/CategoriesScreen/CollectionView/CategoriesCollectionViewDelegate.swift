8bb8244e50b34334a4a2fc3f7ba72c8eb1e8f8a0
Bair <40758299+GhostJavaWriter@users.noreply.github.com>
2023-06-08 21:13:30 -0700

    add animation to categorie cell didSelect

--- RecipeApp/CategoriesScreen/CollectionView/CategoriesCollectionViewDelegate.swift
+++ RecipeApp/CategoriesScreen/CollectionView/CategoriesCollectionViewDelegate.swift
@@ -12,6 +12,17 @@ final class CategoriesCollectionViewDelegate: NSObject, UICollectionViewDelegate
     var navigationController: UINavigationController?
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let cell = collectionView.cellForItem(at: indexPath) else { return }
+        
+        let originalTransform = cell.transform
+        let scaledTransform = originalTransform.scaledBy(x: 0.95, y: 0.95)
+        
+        cell.transform = scaledTransform
+        
+        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0, options: [], animations: {
+            cell.transform = originalTransform
+        }, completion: nil)
+        
         let recipesVC = RecipesListViewController()
         navigationController?.pushViewController(recipesVC, animated: true)
     }
